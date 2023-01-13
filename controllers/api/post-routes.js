927f15ad7e61cb39006db768888c60e421f6663d
Medici <107539009+taholton748@users.noreply.github.com>
2023-01-13 03:09:20 -0700

    changed post_url things

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -5,7 +5,7 @@ const { Post, User, Comment } = require("../../models");
 router.get("/", (req, res) => {
   console.log("======================");
   Post.findAll({
-    attributes: ["id", "post_url", "title", "created_at"],
+    attributes: ["id", "post", "title", "created_at"],
     order: [["created_at", "DESC"]],
     include: [
       {
@@ -34,7 +34,7 @@ router.get("/:id", (req, res) => {
     where: {
       id: req.params.id,
     },
-    attributes: ["id", "post_url", "title", "created_at"],
+    attributes: ["id", "post", "title", "created_at"],
     include: [
       {
         model: Comment,
--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -27,7 +27,7 @@ router.get("/:id", (req, res) => {
     include: [
       {
         model: Post,
-        attributes: ["id", "title", "post_url", "created_at"],
+        attributes: ["id", "title", "post", "created_at"],
       },
       // include the Comment model here:
       {
